67f063ce890efd6db4dbd7f2cb135aeac23d0b01
DanielMoalem <aussiemoalem@gmail.com>
2024-06-18 09:45:52 +0300

    Add files via upload

---
+++ TEST_2024_06_18_06_44_36/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+rectMode(CENTER)
+}
+
+function draw() {
+  background(220);
+  rect (width/2,height/2,100,100) 
+  ellipse (width/2,height/2,50)
+
+}
